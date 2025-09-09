f21ddc758c9d4fb1455b7806481fffbc11c02452
SagarDahibhate <sagardahibhate1@gmail.com>
2025-09-09 11:32:06 +0530

    added hql more handon and structure diagram

--- HQLExamples/src/main/java/com/sagar/FetchDataUsingHql.java
+++ HQLExamples/src/main/java/com/sagar/FetchDataUsingHql.java
@@ -15,27 +15,39 @@ public class FetchDataUsingHql {
 
 		Configuration cfg = new Configuration().configure("hibernate.cfg.xml");
 		SessionFactory factory = cfg.buildSessionFactory();
-		String hql = "from com.sagar.model.Student";
+		
 
 		try (Session session = factory.openSession()) {
 			Transaction transaction = session.beginTransaction();
+			
+			//  simiple select  operation for fetching all columns data
+			String hql = "from com.sagar.model.Student";
 			Query query = session.createQuery(hql);
 			List<Student> list = query.list();
 
 			for (Student s : list) {
 				System.out.println(s);
 			}
-
 			
 			
-			String hql2="select rollNo,name from com.sagar.model.Student";
-			Query q=session.createQuery(hql2);
-			List<Object[]>list2=q.list();
-			for(Object[]ob:list2)
-			{
-			System.out.println(ob[0]+","+ob[1]);
+			
+			// select  operation for fetching more than one column but not all columns of table.
+			String hql2 = "select rollNo,name from com.sagar.model.Student";
+			Query q = session.createQuery(hql2);
+			List<Object[]> list2 = q.list();
+			for (Object[] ob : list2) {
+				System.out.println(ob[0] + "," + ob[1]);
 			}
 
+			
+			// select  operation for fetching only one column of table.
+			String hql3 = "select name from com.sagar.model.Student";
+			Query query2 = session.createQuery(hql3);
+			List<String> list3 = query2.list();
+			for(String s:list3) {
+				System.out.println("name of employees : "+s);
+			}
+			
 			
 			transaction.commit();
 			session.close();
