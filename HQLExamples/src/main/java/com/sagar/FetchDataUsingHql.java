950e646a0f9cc4765362f1aeaa40fa64a6259c87
SagarDahibhate <sagardahibhate1@gmail.com>
2025-09-09 12:02:29 +0530

    added unique method example with hql

--- HQLExamples/src/main/java/com/sagar/FetchDataUsingHql.java
+++ HQLExamples/src/main/java/com/sagar/FetchDataUsingHql.java
@@ -15,12 +15,11 @@ public class FetchDataUsingHql {
 
 		Configuration cfg = new Configuration().configure("hibernate.cfg.xml");
 		SessionFactory factory = cfg.buildSessionFactory();
-		
 
 		try (Session session = factory.openSession()) {
 			Transaction transaction = session.beginTransaction();
-			
-			//  simiple select  operation for fetching all columns data
+
+			// simiple select operation for fetching all columns data
 			String hql = "from com.sagar.model.Student";
 			Query query = session.createQuery(hql);
 			List<Student> list = query.list();
@@ -29,8 +28,8 @@ public class FetchDataUsingHql {
 				System.out.println(s);
 			}
 
-			
-			// select  operation for fetching more than one column but not all columns of table.
+			// select operation for fetching more than one column but not all columns of
+			// table.
 			String hql2 = "select rollNo,name from com.sagar.model.Student";
 			Query q = session.createQuery(hql2);
 			List<Object[]> list2 = q.list();
@@ -38,31 +37,38 @@ public class FetchDataUsingHql {
 				System.out.println(ob[0] + "," + ob[1]);
 			}
 
-			
-			// select  operation for fetching only one column of table.
+			// select operation for fetching only one column of table.
 			String hql3 = "select name from com.sagar.model.Student";
 			Query query2 = session.createQuery(hql3);
 			List<String> list3 = query2.list();
-			for(String s:list3) {
-				System.out.println("name of employees : "+s);
+			for (String s : list3) {
+				System.out.println("name of employees : " + s);
 			}
-			
-			
+
 			// Hibernate 6 ne positional parameters ka pura style change kar diya hai.
-			// Hibernate 6 me ?1 style hata diya gaya hai, ab sirf named parameters hi use karne padte hain.
-			
-			String hql4="from com.sagar.model.Student where rollNo=:roll or name=:name";
-			Query query4=session.createQuery(hql4);
+			// Hibernate 6 me ?1 style hata diya gaya hai, ab sirf named parameters hi use
+			// karne padte hain.
+
+			String hql4 = "from com.sagar.model.Student where rollNo=:roll or name=:name";
+			Query query4 = session.createQuery(hql4);
 			query4.setParameter("roll", 101);
 			query4.setParameter("name", "sagar");
-			
-			List<Student> studList=query4.list();
+
+			List<Student> studList = query4.list();
 			for (Student s : studList) {
 				System.out.println(s);
 			}
 
-			
-			
+//			uniqueResult():- this method is used for select HQL operation. If query returns one row data
+//			    then choose this method instead of query. List() method.
+
+//			• if will save memory, by avoiding list object for one row data.
+			String hql5 = "from com.sagar.model.Student where rollNo=:roll";
+			Query q2 = session.createQuery(hql5);
+			q2.setParameter("roll", 102);
+			Student student = (Student) q2.uniqueResult();
+			System.out.println(student);
+
 			transaction.commit();
 			session.close();
 			factory.close();
